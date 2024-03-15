2f1dd3ffe64fe2f5082b5a9172e2152e3be80484
Shaibmen <ibin2005636@gmail.com>
2024-03-15 20:57:42 +0300

    first commit

---
+++ practika_1_2/Page2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace practika_1_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        private ChurchEntities basadan = new ChurchEntities();
+        public Page2()
+        {
+            InitializeComponent();
+            SecondGrid.ItemsSource = basadan.ChurchService.ToList();
+        }
+    }
+}
